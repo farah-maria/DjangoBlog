7b23fd7055faaf5f71231fb0932426dff43e9eed
Farah M <farah.maria.rahman@gmail.com>
2022-12-04 19:24:29 +0000

    add sign in and sign out and register functionality code in base.html

--- codestar/settings.py
+++ codestar/settings.py
@@ -38,12 +38,22 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'blog',
 ]
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
